fc5344e55bc5272b3a856c62397f688da0f9dc87
ferdinandes guimaraes <fnascimento@fastprobr.com>
2025-01-25 18:18:42 -0300

    fix: deployment pipeline backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,8 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GOOGLE_ID:
-      "941760915080-or42cc6djhae2kqs91kv24cj8ajchlmf.apps.googleusercontent.com",
-    GOOGLE_SECRET: "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",
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
     SECRET: "JWT",
     API_URL: process.env.API_URL || "http://172.29.64.1:4000",
   },
