3ba4a259fb7053ebeb539c352c1bbf31b46931ff
ferdinandes guimaraes <fnascimento@fastprobr.com>
2025-01-26 18:25:33 -0300

    fix: API_URL

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Application } from "express";
 import roomRoutes from "./room.routes";
 import authRoutes from "./auth.routes";
 import gameRoutes from "./game.routes";
-const BASE_API = "/api";
+const BASE_API = "/api/v1";
 export default function routes(app: Application) {
   app.use(`${BASE_API}/rooms`, roomRoutes);
   app.use(`${BASE_API}/game`, gameRoutes);
--- frontend/src/hooks/axios.ts
+++ frontend/src/hooks/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.API_URL}/api`,
+  baseURL: process.env.API_UR,
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache",
