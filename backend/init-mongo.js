32d1781b939a7f502f5732e22c62f4ae943966f8
ferdinandes guimaraes <fnascimento@fastprobr.com>
2025-01-29 17:54:10 -0300

    fix: mongo and nginx configurations

--- backend/init-mongo.js
+++ backend/init-mongo.js
@@ -2,11 +2,7 @@ print("Configuring MongoDB replica set...");
 
 rs.initiate({
   _id: "rs0",
-  members: [
-    { _id: 0, host: "mongodb1:27017" },
-    { _id: 1, host: "mongodb2:27017" },
-    { _id: 2, host: "mongodb3:27017" },
-  ],
+  members: [{ _id: 0, host: "mongodb1:27017" }],
 });
 
 print("Replica Set initiated");
---
+++ init-mongo.js
@@ -0,0 +1,12 @@
+print("Configuring MongoDB replica set...");
+
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongodb1:27017" },
+    { _id: 1, host: "mongodb2:27017" },
+    { _id: 2, host: "mongodb3:27017" },
+  ],
+});
+
+print("Replica Set initiated");
