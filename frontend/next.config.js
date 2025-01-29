36b84af5a2c009c808dd477150143686691807c4
ferdinandes guimaraes <fnascimento@fastprobr.com>
2025-01-28 23:07:59 -0300

    fix: create new vm

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     GOOGLE_SECRET: process.env.GOOGLE_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || "https://carteado.ferdinandes.com.br",
+      process.env.NEXT_PUBLIC_API_URL || "http://carteado.ferdinandes.com.br",
   },
   images: {
     domains: ["lh3.googleusercontent.com", "developers.google.com"],
