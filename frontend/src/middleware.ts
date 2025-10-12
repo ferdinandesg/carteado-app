e5f4160479f74a145ddae31f1c1e3fd31d866242
ferdinandes guimaraes <fnascimento@fastprobr.com>
2025-10-12 19:19:33 -0300

    fix: remove middleware

--- frontend/src/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware";
-
-// Configuração para definir quais rotas serão protegidas
-export const config = {
-  matcher: ["/menu/:path*", "/rooms/:path*", "/room/:path*"],
-};
