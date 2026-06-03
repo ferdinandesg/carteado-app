7cfc5ecaa6b2bebfdd780e8164b9eb945b8bc86d
ferdinandes guimaraes <fnascimento@fastprobr.com>
2026-06-03 15:32:10 -0300

    fix: routine

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,19 @@
 /** @type {import('lint-staged').Configuration} */
 module.exports = {
-  "*.{ts,tsx,js,jsx,json,md,yml,yaml}": "prettier --write",
+  "*.{json,md,yml,yaml}": "prettier --write",
 
-  "backend/src/**/*.ts": (files) =>
-    `eslint --config backend/eslint.config.mjs --fix ${files.join(" ")}`,
+  "backend/src/**/*.ts": [
+    "prettier --write",
+    "eslint --config backend/eslint.config.mjs --fix",
+  ],
 
-  "frontend/src/**/*.{ts,tsx}": (files) =>
-    `eslint --config frontend/eslint.config.mjs --fix ${files.join(" ")}`,
+  "frontend/src/**/*.{ts,tsx}": [
+    "prettier --write",
+    "eslint --config frontend/eslint.config.mjs --fix",
+  ],
 
-  "shared/**/*.ts": () => "npm run ts-check -w shared",
+  "shared/**/*.ts": [
+    "prettier --write",
+    () => "npm run ts-check -w shared",
+  ],
 };
